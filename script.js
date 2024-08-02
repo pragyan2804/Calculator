ee409a7ae992afe0be2a7a2b45f91452a62522be
pragyan2804 <99196461+pragyan2804@users.noreply.github.com>
2024-08-03 02:50:07 +0530

    functionality somewhat working

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let num1 = ""
-let num2 = ""
-let op = ""
-
+let num1 = "";
+let num2 = "";
+let op = "";
 
 // Wait until the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
@@ -13,91 +12,124 @@ document.addEventListener('DOMContentLoaded', function() {
     const numButtons = document.querySelectorAll('.nums');
     const inputBox = document.getElementById('myInput');
 
-    //OPERATION BUTTONS ON
-    function opButtonsON(){
-        opButtons.forEach(opButtons => {
-            opButtons.disabled = false;
+    // OPERATION BUTTONS ON
+    function opButtonsON() {
+        opButtons.forEach(button => {
+            button.disabled = false;
         });
     }
-    //OPERATION BUTTONS OFF
-    function opButtonsOFF(){
-        opButtons.forEach(opButtons => {
-            opButtons.disabled = true;
+
+    // OPERATION BUTTONS OFF
+    function opButtonsOFF() {
+        opButtons.forEach(button => {
+            button.disabled = true;
         });
-    };
-    //PERIOD BUTTON ON
-    function periodButtonON(){
-        periodButton.forEach(periodButton => {
-        periodButton.disabled = false;
-    });
     }
-    //PERIOD BUTTON OFF
-    function periodButtonOFF(){
-        periodButton.forEach(periodButton => {
-        periodButton.disabled = true;
-    });
+
+    // PERIOD BUTTON ON
+    function periodButtonON() {
+        periodButton.forEach(button => {
+            button.disabled = false;
+        });
+    }
+
+    // PERIOD BUTTON OFF
+    function periodButtonOFF() {
+        periodButton.forEach(button => {
+            button.disabled = true;
+        });
+    }
+
+    // CLEAR SCREEN
+    function clrscn() {
+        inputBox.value = "";
+    }
+
+    // OPERATION FUNCTION
+    function operation() {
+        switch (op) {
+            case "+":
+                num1 = parseFloat(num1) + parseFloat(num2);
+                break;
+            case "-":
+                num1 = parseFloat(num1) - parseFloat(num2);
+                break;
+            case "x":
+                num1 = parseFloat(num1) * parseFloat(num2);
+                break;
+            case "/":
+                num1 = parseFloat(num1) / parseFloat(num2);
+                break;
+        }
+        inputBox.value = num1;
+        num2 = "";
+        opButtonsOFF();
     }
 
     opButtonsOFF();
     periodButtonOFF();
+
     // NUMBER BUTTONS
     numButtons.forEach(button => {
         button.addEventListener('click', function() {
             opButtonsON();
             // Get the data-text attribute value from the clicked button
-            const newText = parseInt(button.getAttribute('data-text'));
-            // Change the value of the input box to the text from the clicked button
-            inputBox.value = inputBox.value + newText;
+            const newText = button.getAttribute('data-text');
+            // If an operation has already been chosen, we're entering num2
+            if (op === "") {
+                num1 += newText;
+                inputBox.value = num1;
+            } else {
+                num2 += newText;
+                inputBox.value = num2;
+            }
         });
     });
+
     // OPERATION BUTTONS
     opButtons.forEach(button => {
         button.addEventListener('click', function() {
             // Get the data-text attribute value from the clicked button
-            const newText = (button.getAttribute('data-text'));
-            // INSERT OPERATION SWITCH CASES HERE
-            function operation(num1, num2, newText){
-                switch(newText){
-                    case "+":
-                        num1+=newText;
-                    case "-":
-                        num1-=newText;
-                    case "x":
-                        num1*=newText;
-                    case "/":
-                        num1/=newText;
-                }
-            }     
-            inputBox.value = inputBox.value + newText;
-            button.disabled= true;     
+            op = button.getAttribute('data-text');
+            button.disabled = true;     
             periodButtonON();
+            clrscn();
         });
     });
+
     // PERIOD BUTTON
     periodButton.forEach(button => {
         button.addEventListener('click', function() {
             // Get the data-text attribute value from the clicked button
-            const newText = (button.getAttribute('data-text'));
-            // Change the value of the input box to the text from the clicked button
-            inputBox.value = inputBox.value + newText;
+            const newText = button.getAttribute('data-text');
+            // Append the period to the current number
+            if (op === "") {
+                num1 += newText;
+                inputBox.value = num1;
+            } else {
+                num2 += newText;
+                inputBox.value = num2;
+            }
             periodButtonOFF();
         });
     });
+
     // AC BUTTON
     ACbutton.forEach(button => {
         button.addEventListener('click', function() {
-            num1 = " "
-            num2 = " "
-            op = " "
-            newText=" "
-            inputBox.value = newText;
+            num1 = "";
+            num2 = "";
+            op = "";
+            clrscn();
+            opButtonsOFF();
+            periodButtonOFF();
         });
     });
+
     // EQUALS BUTTON
-    eqButtonutton.forEach(button => {
+    eqButton.forEach(button => {
         button.addEventListener('click', function() {
-            inputBox.value = num1;
+            operation();
         });
     });
 });
-
